3c34479dc0becd8a50e703edde8b902a596f22e2
Dustin <khanhduyk16tpm@gmail.com>
2021-05-21 10:09:47 +0700

    remove old files

---
+++ src/assets/js/main.js
@@ -0,0 +1,33 @@
+var sidebarToggle = new NoCallbackToggle("#menu-btn",{
+    onOn: function(n) {
+      var sidebar = document.querySelector("#sidebar");
+      var container = document.querySelector(".my-container");
+      var sidebarbtnon = document.querySelector(".btn-sidebar-on");
+      sidebar.classList.add("active-nav");
+      container.classList.add("active-cont");
+      sidebarbtnon.classList.remove("d-none");
+      n.classList.add("d-none");
+      const nClass = n.classList;
+      if (nClass.contains("toggle-on")){
+        n.classList.remove("toggle-on");
+        n.classList.add("toggle-off");
+      }
+    },
+    onOff: function(n) {
+      var sidebar = document.querySelector("#sidebar");
+      var container = document.querySelector(".my-container");
+      var sidebarbtnoff = document.querySelector(".btn-sidebar-off");
+      sidebar.classList.remove("active-nav");
+      container.classList.remove("active-cont");
+      n.classList.add("d-none");
+      const nClass = n.classList;
+      if (nClass.contains("toggle-off")){
+        n.classList.remove("toggle-off");
+        n.classList.add("toggle-on");
+      }
+      sidebarbtnoff.classList.remove("d-none");
+    },
+    success: function() {
+        return !0
+    }
+});
