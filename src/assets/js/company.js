65cd50fbed59f37dcc78ba5df1125188f48ddf8e
Dustin <khanhduyk16tpm@gmail.com>
2021-05-31 16:23:24 +0700

    remove unnecessary css

--- src/assets/js/company.js
+++ src/assets/js/company.js
@@ -1,23 +1,47 @@
-const form = new JaxForm('#filterCompanyForm', {
-    container: '#results',
-    nextBtn: '#next',
-    loadingIndicator: '#loading',
-    endIndicator: '#end',
-    xhr: true,
-    request: function (f, page) {
-        $.ajax({
-            type: f.formAttribute('method'),
-            url: f.formAttribute('action') + '/' + page,
-            contentType: 'application/json',
-            dataType: 'json',
-            data: JSON.stringify(f.formData(true)),
-            success: function (res) {
-                if (res.status === 'success') {
-                    document.querySelector('#total').innerHTML = res.total_records;
-                    f.setMaxPerPage(res.max_per_page);
-                    f.addContent(res.content, res.total_records);
-                }
-            }
-        });
+var editToggle = new NoCallbackToggle(".edit-btn",{
+    onOn: function(n) {
+      n.classList.add("d-none");
+      n.nextElementSibling.classList.remove("d-none");
+
+      var inputs = document.getElementsByClassName(n.getAttribute('data-input-class'));
+      for (var i = 0; i < inputs.length; i++) {
+       inputs.item(i).classList.remove("d-none")
+      }
+    
+      var texts = document.getElementsByClassName(n.getAttribute('data-text-class'));
+      for (var i = 0; i < texts.length; i++) {
+       texts.item(i).classList.add("d-none")
+      }
+    },
+    success: function() {
+        return !0
+    }
+});
+var saveToggle = new Toggle(".save-btn",{
+    onOn: function(n) {
+      n.classList.add("d-none");
+      n.previousElementSibling.classList.remove("d-none");
+      n.previousElementSibling.classList.remove("toggle-on");
+ 
+      var inputs = document.getElementsByClassName(n.getAttribute('data-input-class'));
+      for (var i = 0; i < inputs.length; i++) {
+       inputs.item(i).classList.add("d-none")
+       var textEl = document.getElementById(inputs.item(i).getAttribute('data-text-id'));
+       textEl.innerHTML = inputs.item(i).value
+       textEl.value = inputs.item(i).value
+       textEl.classList.remove("d-none")
+      }
+    },
+    callback: function(n) {
+        // $.ajax({
+        //     type: "get",
+        //     url: a.dataset.url,
+        //     contentType: "application/json",
+        //     dataType: "json",
+        //     success: function(n) {
+        //         "success" == n.status && ("add" == n.action ? apply.triggerOn(a) : "delete" == n.action && apply.triggerOff(a))
+        //     }
+        // })
+        saveToggle.triggerOn(n)
     }
 });
